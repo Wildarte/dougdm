a8e34cd1058026fbe70974d06df7f64c07288729
wildarte <wildarte10@gmail.com>
2021-11-22 19:59:02 -0300

    comecei a fazer o slider dos cards de serviço

--- assets/js/card-service.js
+++ assets/js/card-service.js
@@ -1,15 +1,14 @@
 var current_pos = 0;
 var limit_cards = 0;
 var count_cards = document.querySelectorAll(".type_services .type_service_body .card_service");
+
 for(var i = 0; i < count_cards.length-4; i++){
     limit_cards -= 25;
 }
-console.log("valor total: " + limit_cards);
 document.getElementById("btn-next-service").addEventListener("click", function(){
     if(current_pos > limit_cards){
         current_pos -= 25;
         var els = document.querySelectorAll(".type_services .type_service_body .card_service");
-        console.log("antigo valor: "+current_pos+"%");
         for(var i = 0; i < els.length; i++){
             els[i].style.left = current_pos+"%";
         }
@@ -19,13 +18,11 @@ document.getElementById("btn-next-service").addEventListener("click", function()
             document.getElementById("btn-previous-service").style.display = "none";
         }
     }
-    console.log("novo valor: "+current_pos);
 });
 
 document.getElementById("btn-previous-service").addEventListener("click", function(){
     
     var els = document.querySelectorAll(".type_services .type_service_body .card_service");
-    console.log("antigo valor: "+current_pos+"%");
 
     if(current_pos < 0){
         current_pos += 25;
@@ -38,5 +35,4 @@ document.getElementById("btn-previous-service").addEventListener("click", functi
             document.getElementById("btn-previous-service").style.display = "none";
         }
     }
-    console.log("novo valor: "+current_pos);
 });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-var i = 0;
-var txt = 'Ouvir. Entender. Melhorar.';
+var n = 0;
+var txt = "Ouvir. Entender. Melhorar.";
 var speed = 100;
 
 function typeWriter() {
-    if (i < txt.length) {
-        document.getElementById("title_intro").innerHTML += txt.charAt(i);
-        i++;
+    if (n < txt.length) {
+        document.getElementById("title_intro").innerHTML += txt.charAt(n);
+        n++;
         setTimeout(typeWriter, speed);
     }
 }
